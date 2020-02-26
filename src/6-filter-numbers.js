6e640db80a8386da9b58b2d89516c2e97c715679
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-27 00:02:22 +0100

    done with 6

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,11 @@
 export const filterNumbers = (array, largerThan) => {
+
+  array.forEach((item, index) => {
+    if (item > largerThan) {
+      let numbersToRemove = array.length - index
+      array.splice(index, numbersToRemove);
+    }
+  })
+
   return array
 }
