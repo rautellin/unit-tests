6fc1f98e10dd9e6d1c4ce987910a6bb040b029bb
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-27 19:43:59 +0100

    step 8 done

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,9 @@
 export const hackerSpeak = (words) => {
-  return words
+  let text = words
+  text = text.replace(/a/gi, "4")
+  text = text.replace(/e/gi, "3")
+  text = text.replace(/i/gi, "1")
+  text = text.replace(/o/gi, "0")
+  text = text.replace(/s/gi, "5")
+  return text
 }
