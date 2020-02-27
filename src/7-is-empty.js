cf5b1519db9153b94a3c99592586fa688b495372
Nathalie Rautell <nathalierautell@Nathalies-MacBook-Pro.local>
2020-02-27 19:29:29 +0100

    step 7 done

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -2,8 +2,16 @@ export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
   if (type === 'string') {
-    return stringArrayOrObject === ''
+    if (stringArrayOrObject === '') {
+      return true
+    }
+    return false
+  } else if (type === 'object') {
+    let objectLength = Object.keys(stringArrayOrObject).length
+    if (objectLength == 0) {
+      return true
+    }
+    return false
   }
-
   return false
 }
